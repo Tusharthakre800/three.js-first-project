209bfe950037dc4f478252f130d763c44b44a7be
tushar <tusharthakre800@gmail.com>
2025-09-10 23:48:28 +0530

    new code may its work

--- three_project/vite.config.js
+++ three_project/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
+// import glsl from 'vite-plugin-';
 
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
+    // plugins : [glsl()]
+    base: './',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
     }
-  }
 })
